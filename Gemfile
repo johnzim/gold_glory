7f9a2bba4cbd096cfec44cee27defbb3a63f8130
John Louis Swaine <jlswaine@me.com>
2011-02-06 20:32:36 +0000

    Stuff

--- Gemfile
+++ Gemfile
@@ -1,15 +1,15 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.0.4.rc1'
+gem 'rails'
 gem 'sqlite3-ruby', '1.3.2', :require => 'sqlite3'
 
 group :development do
-  gem 'rspec-rails', '2.4.1'
+  gem 'rspec-rails'
 end
 
 group :test do
-  gem 'rspec', '2.4.0'
-  gem 'webrat', '0.7.1'
+  gem 'rspec'
+  gem 'webrat'
 end
 
 # Bundle edge Rails instead:
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 GoldGlory::Application.routes.draw do
+
+
+
+
+  get "pages/home"
+
+  get "pages/contact"
+
+  get "pages/about"
+
+  match '/contact', :to => "pages#contact"
+  match '/about', :to => "pages#about"
+
+  root :to => "pages#home"
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +64,6 @@ GoldGlory::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
 
   # See how all your routes lay out with "rake routes"
 
