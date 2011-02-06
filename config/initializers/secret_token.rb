c69108b5f8d0ac7cef648a48810199f38a1db67b
John Louis Swaine <jlswaine@me.com>
2011-02-06 19:50:57 +0000

    first Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoldGlory::Application.config.secret_token = 'b43e708ddeff98cd605d38a7ccfd286102f652456aa008bb946fe5a2bad5a1c0b89ef7f1a8a4ce7275239ea4c08ecd09a1eec0457077d7351f3e215b750a2476'
