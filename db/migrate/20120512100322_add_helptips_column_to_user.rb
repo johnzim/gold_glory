713133e88c896556c15a37a55a34478aabe7bbb2
John Louis <jlswaine@me.com>
2012-05-12 11:05:43 +0100

    Coach buying system updated - n00b coaches purchasable. Activated tab bar link to Marketplace.

---
+++ db/migrate/20120512100322_add_helptips_column_to_user.rb
@@ -0,0 +1,9 @@
+class AddHelptipsColumnToUser < ActiveRecord::Migration
+  def self.up
+    add_column :users, :help_tips, :boolean
+  end
+
+  def self.down
+    remove_column :users, :help_tips
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120507195043) do
+ActiveRecord::Schema.define(:version => 20120512100322) do
 
   create_table "athletes", :force => true do |t|
     t.integer  "user_id"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(:version => 20120507195043) do
     t.boolean  "creation_mode"
     t.boolean  "admin"
     t.integer  "tp_total"
+    t.boolean  "help_tips"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
